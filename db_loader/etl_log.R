ab579ddddbf62b49c544ce401d7c9a185221f3f6
Whitehurst <jwhitehurst@ph.lcl>
2020-12-23 20:41:56 -0800

    updated etl_log

--- db_loader/etl_log.R
+++ db_loader/etl_log.R
@@ -12,21 +12,40 @@
 #### FUNCTION CREATE ETL LOG ####
 create_etl_log_f <- function(
   conn,
+  config,
   batch_name,
   file_name,
   geo_type,
   geo_year,
-  year) {
+  year,
+  r_type) {
   
   ### SET DATABASE SETTINGS
   etl_shema <- "metadata"
   etl_table <- "pop_etl_log"
   
-  ### CHECK FOR EXISITING ENTRIES
-   <- DBI::dbGetQuery(conn, 
-                            glue::glue_sql("SELECT TOP (1) * FROM {`schema`}.{`table`}
-                             ORDER BY etl_batch_id DESC",
-                                           .con = conn))
+  ### CHECK FOR EXISITING ENTRIES 
+  ### TO DO
+  #etl_match <- DBI::dbGetQuery(conn, glue::glue_sql(
+  #  "SELECT TOP (1) * FROM {`etl_schema`}.{`etl_table`} ORDER BY etl_batch_id DESC",
+  #  .con = conn))
   
+  ### CREATE NEW ETL BATCH ID
+  sql_load <- glue::glue_sql(
+    "INSERT INTO {`etl_schema`}.{`etl_table`} 
+    (batch_name, file_name, geo_type, geo_scope, geo_year, year, r_type) 
+    VALUES ({`batch_name`}, {`file_name`}, {`geo_type`}, {`geo_scope`}, 
+    {`geo_year`}, {`year`}, {`r_type`})", 
+    .con = conn)
+  DBI::dbGetQuery(conn, sql_load)
   
+  ### GET NEW ETL BATCH ID
+  etl_batch_id <- DBI::dbGetQuery(conn, glue::glue_sql(
+    "SELECT TOP (1) * FROM {`etl_schema`}.{`etl_table`} 
+    WHERE batch_name = {`batch_name`} AND file_name = {`file_name`} 
+    AND geo_type = {`geo_type`} AND geo_scope = {`geo_scope`} 
+    AND geo_year = {`geo_year`} AND year = {`year`} AND r_type = {`r_type`}
+    ORDER BY etl_batch_id DESC",
+    .con = conn))
+  return(etl_batch_id)
 }
