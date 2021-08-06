45f57a952dff0387894e246edcc9e3320f595219
Whitehurst <jwhitehurst@ph.lcl>
2021-08-06 11:44:40 -0700

    add function file

---
+++ db_loader/create_db_connection.R
@@ -0,0 +1,59 @@
+#### CODE TO SET CURRENT DB CONNECTION
+# Jeremy Whitehurst, PHSKC (APDE)
+#
+# 2020-12
+
+
+### Run from master_mcaid_partial script
+# https://github.com/PHSKC-APDE/claims_data/blob/master/claims_db/db_loader/mcaid/master_mcaid_partial.R
+
+### Function elements
+# server = whether we are working in HHSAW or PHClaims or other
+
+
+#### FUNCTION ####
+create_db_connection <- function(server = c("phclaims", "hhsaw", "inthealth", "APDEStore"), 
+                                 prod = T, 
+                                 interactive = F) {
+  
+  server <- match.arg(server)
+  
+  if (server == "hhsaw") {
+    db_name <- "hhs_analytics_workspace"
+  } else if (server == "inthealth") {
+    db_name <- "inthealth_edw"
+  }
+  
+  if (prod == T & server %in% c("hhsaw", "inthealth")) {
+    server_name <- "tcp:kcitazrhpasqlprp16.azds.kingcounty.gov,1433"
+  } else {
+    server_name <- "tcp:kcitazrhpasqldev20.database.windows.net,1433"
+  }
+  
+  if (server == "phclaims") {
+    conn <- DBI::dbConnect(odbc::odbc(), "PHClaims51")
+  } else if (server == "APDEStore") {
+    conn <- DBI::dbConnect(odbc::odbc(), "PH_APDEStore51")
+  } else if (interactive == F) {
+    conn <- DBI::dbConnect(odbc::odbc(),
+                           driver = "ODBC Driver 17 for SQL Server",
+                           server = server_name,
+                           database = db_name,
+                           uid = keyring::key_list("hhsaw_dev")[["username"]],
+                           pwd = keyring::key_get("hhsaw_dev", keyring::key_list("hhsaw_dev")[["username"]]),
+                           Encrypt = "yes",
+                           TrustServerCertificate = "yes",
+                           Authentication = "ActiveDirectoryPassword")
+  } else if (interactive == T) {
+    conn <- DBI::dbConnect(odbc::odbc(),
+                           driver = "ODBC Driver 17 for SQL Server",
+                           server = server_name,
+                           database = db_name,
+                           uid = keyring::key_list("hhsaw_dev")[["username"]],
+                           Encrypt = "yes",
+                           TrustServerCertificate = "yes",
+                           Authentication = "ActiveDirectoryInteractive")
+  }
+  
+  return(conn)
+}
